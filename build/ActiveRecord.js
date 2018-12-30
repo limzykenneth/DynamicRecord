6dd108bcd7e1edc2bfd74aaba599f31947b7c624
limzykenneth <limzy.kenneth@gmail.com>
2018-12-30 21:27:01 +0000

    ES6 and Typescript stuff

--- build/ActiveRecord.js
+++ build/ActiveRecord.js
@@ -7,141 +7,144 @@ const ActiveCollection = require("./ActiveCollection.js");
 const ActiveSchema = require("./ActiveSchema.js");
 // Let's get mongodb working first
 const connect = require("./mongoConnection.js")(process.env.mongo_server, process.env.mongo_db_name, process.env.mongo_user, process.env.mongo_pass);
-const ActiveRecord = function (options) {
-    let tableSlug = options.tableSlug;
-    let tableName = options.tableName || options.tableSlug;
-    let _db;
-    this._databaseConnection = connect;
-    let _schema;
-    _schema = this.Schema = new (ActiveSchema(this._databaseConnection))();
-    var _ready = this._ready = connect.then((db) => {
-        _db = this._db = db;
-        return db.createCollection(tableSlug).then((col) => {
-            this._tableName = tableName;
-            return Promise.resolve(col);
+class ActiveRecord {
+    constructor(options) {
+        let tableSlug = options.tableSlug;
+        let tableName = options.tableName || options.tableSlug;
+        let _db;
+        this._databaseConnection = connect;
+        let _schema;
+        _schema = this.Schema = new (ActiveSchema(this._databaseConnection))();
+        var _ready = this._ready = connect.then((db) => {
+            _db = this._db = db;
+            return db.createCollection(tableSlug).then((col) => {
+                this._tableName = tableName;
+                return Promise.resolve(col);
+            });
         });
-    });
-    var Model = this.Model = function (data, _preserveOriginal) {
-        this.data = data;
-        if (_preserveOriginal) {
-            this._original = _.cloneDeep(data);
-        }
-        else {
-            this._original = null;
-        }
-    };
-    Model.prototype.save = function () {
-        return _ready.then((col) => {
-            if (this._original) {
-                return col.updateOne(this._original, this.data, { upsert: true }).then((result) => {
-                    this._original = _.cloneDeep(this.data);
-                    return Promise.resolve(col);
-                });
+        var Model = this.Model = function (data, _preserveOriginal) {
+            this.data = data;
+            if (_preserveOriginal) {
+                this._original = _.cloneDeep(data);
             }
             else {
-                // Check if collection contains index that needs auto incrementing
-                return _db.collection("_counters").findOne({ collection: tableSlug }).then((res) => {
-                    let promises = [];
-                    if (res !== null) {
-                        // Auto incrementing index exist
-                        _.each(res.sequences, (el, columnLabel) => {
-                            promises.push(_schema._incrementCounter(tableSlug, columnLabel).then((newSequence) => {
-                                this.data[columnLabel] = newSequence;
-                                return Promise.resolve(newSequence);
-                            }));
-                        });
-                        return Promise.all(promises);
-                    }
-                    else {
-                        // No auto incrementing index
-                        return Promise.resolve();
-                    }
-                }).then(() => {
-                    // Save data into the database
-                    return col.insertOne(this.data).then((result) => {
+                this._original = null;
+            }
+        };
+        Model.prototype.save = function () {
+            return _ready.then((col) => {
+                if (this._original) {
+                    return col.updateOne(this._original, this.data, { upsert: true }).then((result) => {
                         this._original = _.cloneDeep(this.data);
                         return Promise.resolve(col);
                     });
+                }
+                else {
+                    // Check if collection contains index that needs auto incrementing
+                    return _db.collection("_counters").findOne({ collection: tableSlug }).then((res) => {
+                        let promises = [];
+                        if (res !== null) {
+                            // Auto incrementing index exist
+                            _.each(res.sequences, (el, columnLabel) => {
+                                promises.push(_schema._incrementCounter(tableSlug, columnLabel).then((newSequence) => {
+                                    this.data[columnLabel] = newSequence;
+                                    return Promise.resolve(newSequence);
+                                }));
+                            });
+                            return Promise.all(promises);
+                        }
+                        else {
+                            // No auto incrementing index
+                            return Promise.resolve();
+                        }
+                    }).then(() => {
+                        // Save data into the database
+                        return col.insertOne(this.data).then((result) => {
+                            this._original = _.cloneDeep(this.data);
+                            return Promise.resolve(col);
+                        });
+                    });
+                }
+            });
+        };
+        Model.prototype.destroy = function () {
+            return _ready.then((col) => {
+                if (this._original) {
+                    return col.deleteOne(this._original).then((result) => {
+                        this._original = null;
+                        this.data = null;
+                        return Promise.resolve(col);
+                    });
+                }
+                else {
+                    throw new Error("Model not saved in database yet.");
+                }
+            });
+        };
+        Model.prototype.validate = function (schema) {
+            var result = false;
+            _.each(this.data, (el, key) => {
+                var field = _.find(schema, (column) => {
+                    return column.label == key;
                 });
-            }
+                if (field.type == "string") {
+                    result = _.isString(el);
+                }
+                else if (field.type == "int") {
+                    result = Number.isInteger(el);
+                }
+            });
+            return result;
+        };
+    }
+    closeConnection() {
+        // Should only ever be called to terminate the node process
+        this._ready.then((col) => {
+            this._db.close();
+        }).catch((err) => {
+            // BY ANY MEANS NECESSARY
+            this._db.close();
         });
-    };
-    Model.prototype.destroy = function () {
-        return _ready.then((col) => {
-            if (this._original) {
-                return col.deleteOne(this._original).then((result) => {
-                    this._original = null;
-                    this.data = null;
-                    return Promise.resolve(col);
+    }
+    findBy(query) {
+        return this._ready.then((col) => {
+            return col.findOne(query).then((model) => {
+                return Promise.resolve(new this.Model(model, true));
+            });
+        });
+    }
+    where(query, orderBy) {
+        return this._ready.then((col) => {
+            return col.find(query).toArray().then((models) => {
+                if (orderBy) {
+                    models = _.sortBy(models, orderBy);
+                }
+                var results = new ActiveCollection();
+                _.each(models, (model, i) => {
+                    results.push(new this.Model(model, true));
                 });
-            }
-            else {
-                throw new Error("Model not saved in database yet.");
-            }
-        });
-    };
-    Model.prototype.validate = function (schema) {
-        var result = false;
-        _.each(this.data, (el, key) => {
-            var field = _.find(schema, (column) => {
-                return column.label == key;
+                return Promise.resolve(results);
             });
-            if (field.type == "string") {
-                result = _.isString(el);
-            }
-            else if (field.type == "int") {
-                result = Number.isInteger(el);
-            }
         });
-        return result;
-    };
-};
-ActiveRecord.prototype.closeConnection = function () {
-    // Should only ever be called to terminate the node process
-    this._ready.then((col) => {
-        this._db.close();
-    }).catch((err) => {
-        // BY ANY MEANS NECESSARY
-        this._db.close();
-    });
-};
-ActiveRecord.prototype.findBy = function (query) {
-    return this._ready.then((col) => {
-        return col.findOne(query).then((model) => {
-            return Promise.resolve(new this.Model(model, true));
-        });
-    });
-};
-ActiveRecord.prototype.where = function (query, orderBy) {
-    return this._ready.then((col) => {
-        return col.find(query).toArray().then((models) => {
-            if (orderBy) {
-                models = _.sortBy(models, orderBy);
-            }
-            var results = new ActiveCollection();
-            _.each(models, (model, i) => {
-                results.push(new this.Model(model, true));
+    }
+    all() {
+        return this._ready.then((col) => {
+            return col.find().toArray().then((models) => {
+                var results = new ActiveCollection();
+                _.each(models, (model, i) => {
+                    results.push(new this.Model(model, true));
+                });
+                return Promise.resolve(results);
             });
-            return Promise.resolve(results);
         });
-    });
-};
-ActiveRecord.prototype.all = function () {
-    return this._ready.then((col) => {
-        return col.find().toArray().then((models) => {
-            var results = new ActiveCollection();
-            _.each(models, (model, i) => {
-                results.push(new this.Model(model, true));
+    }
+    first() {
+        return this._ready.then((col) => {
+            return col.findOne().then((model) => {
+                return Promise.resolve(new this.Model(model, true));
             });
-            return Promise.resolve(results);
         });
-    });
-};
-ActiveRecord.prototype.first = function () {
-    return this._ready.then((col) => {
-        return col.findOne().then((model) => {
-            return Promise.resolve(new this.Model(model, true));
-        });
-    });
-};
+    }
+}
+ActiveRecord.ActiveSchema = new (ActiveSchema(connect))();
 module.exports = ActiveRecord;
--- build/ActiveSchema.js
+++ build/ActiveSchema.js
@@ -4,258 +4,248 @@ require("dotenv").config();
 const Promise = require("bluebird");
 const _ = require("lodash");
 let con;
+{ }
 // Let's get mongodb working first
-// class Schema {
-// 	constructor(){
-// 		this.tableName = null;
-// 		this.tableSlug = null;
-// 		this.definition = [];
-// 	}
-// }
-let Schema = function () {
-    this.tableName = null;
-    this.tableSlug = null;
-    this.definition = [];
-};
-Schema.prototype.createTable = function (options) {
-    let tableSlug = options.tableSlug; // String
-    let tableName = options.tableName; // String
-    let indexColumns = options.indexColumns; // Array
-    // Maybe just let the user handle it themselves?
-    let autoIndex = options.autoIndex; // Boolean (on hold)
-    if (!tableName) {
-        tableName = tableSlug;
-    }
-    return con.then((db) => {
-        let promises = [];
-        promises.push(db.createCollection(tableSlug).then((col) => {
-            this.tableName = tableName;
-            this.tableSlug = tableSlug;
-            return Promise.resolve(db);
-        }));
-        promises.push(db.createCollection("_counters").then((col) => {
-            return col.indexExists("collection").then((result) => {
-                if (result === false) {
-                    return col.createIndex("collection", { unique: true }).then(() => {
-                        return Promise.resolve();
-                    });
-                }
-                else {
-                    return Promise.resolve();
-                }
-            }).then(() => {
-                return col.insertOne({
-                    collection: tableSlug,
-                    sequences: {}
-                }).then(() => {
-                    return Promise.resolve(db);
-                });
-            });
-        }));
-        promises.push(db.collection("_schema").insertOne({
-            collectionSlug: tableSlug,
-            collectionName: tableName,
-            fields: []
-        }));
-        return Promise.all(promises);
-    }).then(() => {
-        if (indexColumns) {
-            if (Array.isArray(indexColumns)) {
-                let promises = [];
-                _.each(indexColumns, (el, i) => {
-                    promises.push(this.addIndex({
-                        name: el.name,
-                        unique: el.unique
-                    }));
-                });
-                return Promise.all(promises);
-            }
-            else {
-                return this.addIndex({
-                    name: indexColumns.name,
-                    unique: indexColumns.unique
-                });
-            }
-        }
-        else {
-            return Promise.resolve();
-        }
-    }).catch((err) => {
+class Schema {
+    constructor() {
         this.tableName = null;
         this.tableSlug = null;
-        throw err;
-    });
-};
-// Add index
-Schema.prototype.addIndex = function (options) {
-    let columnName = options.name;
-    let isAutoIncrement = options.autoIncrement;
-    let unique = options.unique;
-    if (isAutoIncrement && unique === false) {
-        console.warn("Auto increment index must be unique, setting to unique.");
-        unique = true;
+        this.definition = [];
     }
-    if (typeof unique === "undefined") {
-        unique = true;
-    }
-    return con.then((db) => {
-        return db.collection(this.tableSlug).createIndex(columnName, { unique: unique, name: columnName });
-    }).then(() => {
-        if (isAutoIncrement) {
-            return this._setCounter(this.tableSlug, columnName);
+    createTable(options) {
+        let tableSlug = options.tableSlug;
+        let tableName = options.tableName;
+        let indexColumns = options.indexColumns; // Array
+        if (!tableName) {
+            tableName = tableSlug;
         }
-        else {
+        return con.then((db) => {
+            let promises = [];
+            promises.push(db.createCollection(tableSlug).then((col) => {
+                this.tableName = tableName;
+                this.tableSlug = tableSlug;
+                return Promise.resolve(db);
+            }));
+            promises.push(db.createCollection("_counters").then((col) => {
+                return col.indexExists("collection").then((result) => {
+                    if (result === false) {
+                        return col.createIndex("collection", { unique: true }).then(() => {
+                            return Promise.resolve();
+                        });
+                    }
+                    else {
+                        return Promise.resolve();
+                    }
+                }).then(() => {
+                    return col.insertOne({
+                        collection: tableSlug,
+                        sequences: {}
+                    }).then(() => {
+                        return Promise.resolve(db);
+                    });
+                });
+            }));
+            promises.push(db.collection("_schema").insertOne({
+                collectionSlug: tableSlug,
+                collectionName: tableName,
+                fields: []
+            }));
+            return Promise.all(promises);
+        }).then(() => {
+            if (indexColumns) {
+                if (Array.isArray(indexColumns)) {
+                    let promises = [];
+                    _.each(indexColumns, (el, i) => {
+                        promises.push(this.addIndex({
+                            name: el.name,
+                            unique: el.unique,
+                            autoIncrement: el.autoIncrement
+                        }));
+                    });
+                    return Promise.all(promises);
+                }
+                else {
+                    return this.addIndex({
+                        name: indexColumns.name,
+                        unique: indexColumns.unique,
+                        autoIncrement: indexColumns.autoIncrement
+                    });
+                }
+            }
+            else {
+                return Promise.resolve();
+            }
+        }).catch((err) => {
+            this.tableName = null;
+            this.tableSlug = null;
+            throw err;
+        });
+    }
+    addIndex(options) {
+        let columnName = options.name;
+        let isAutoIncrement = options.autoIncrement;
+        let unique = options.unique;
+        if (isAutoIncrement && unique === false) {
+            console.warn("Auto increment index must be unique, setting to unique.");
+            unique = true;
+        }
+        if (typeof unique === "undefined") {
+            unique = true;
+        }
+        return con.then((db) => {
+            return db.collection(this.tableSlug).createIndex(columnName, { unique: unique, name: columnName });
+        }).then(() => {
+            if (isAutoIncrement) {
+                return this._setCounter(this.tableSlug, columnName);
+            }
+            else {
+                return Promise.resolve();
+            }
+        }).catch((err) => {
+            throw err;
+        });
+    }
+    renameIndex(columnName, newColumnName) {
+        // Maybe drop index then recreate but do consider why you need to do this
+    }
+    removeIndex(columnName) {
+        return con.then((db) => {
+            return db.collection(this.tableSlug).dropIndex(columnName);
+        }).catch((err) => {
+            throw err;
+        });
+    }
+    read(tableSlug) {
+        return con.then((db) => {
+            return db.collection("_schema").findOne({ collectionSlug: tableSlug });
+        }).then((data) => {
+            this.tableName = data.collectionName;
+            this.tableSlug = data.collectionSlug;
+            this.definition = data.fields;
             return Promise.resolve();
-        }
-    }).catch((err) => {
-        throw err;
-    });
-};
-// Rename index
-Schema.prototype.renameIndex = function (columnName, newColumnName) {
-    // Maybe drop index then recreate but do consider why you need to do this
-};
-// Remove index
-Schema.prototype.removeIndex = function (columnName) {
-    return con.then((db) => {
-        return db.collection(this.tableSlug).dropIndex(columnName);
-    }).catch((err) => {
-        throw err;
-    });
-};
-// Read and define schema
-Schema.prototype.read = function (tableSlug) {
-    return con.then((db) => {
-        return db.collection("_schema").findOne({ collectionSlug: tableSlug });
-    }).then((data) => {
-        this.tableName = data.collectionName;
-        this.tableSlug = data.collectionSlug;
-        this.definition = data.fields;
-        return Promise.resolve();
-    }).catch((err) => {
-        throw err;
-    });
-};
-Schema.prototype.define = function (tableName, tableSlug, def) {
-    var oldTableName = this.tableName;
-    var oldTableSlug = this.tableSlug;
-    var oldDef = this.definition;
-    this.tableName = tableName;
-    this.tableSlug = tableSlug;
-    this.definition = def;
-    // Create schema in RMDB, do nothing in NoSQL
-    return con.then((db) => {
-        return db.collection("_schema").insertOne({
-            collectionSlug: tableSlug,
-            collectionName: tableName,
-            fields: def
+        }).catch((err) => {
+            throw err;
         });
-    }).catch((err) => {
-        this.tableName = oldTableName;
-        this.tableSlug = oldTableSlug;
-        this.definition = oldDef;
-        throw err;
-    });
-};
-// Columns functions
-Schema.prototype.addColumn = function (label, type, options = {}) {
-    // let isUUIDv4 = options.uuid || false;
-    // let isAutoIncrement = options.autoIncrement || false;
-    // if(isUUIDv4 && isAutoIncrement){
-    // 	throw new Error("Column cannot be both UUID and Auto Increment.");
-    // }
-    // if(isUUIDv4) type = "uuid";
-    // if(isAutoIncrement) type = "int";
-    this.definition.push({
-        name: label,
-        slug: label,
-        type: type
-    });
-    return this._writeSchema().catch((err) => {
-        this.definition.pop();
-        throw err;
-    });
-};
-Schema.prototype.renameColumn = function (slug, newSlug) {
-    var index = _.findIndex(this.definition, (el) => {
-        return el.slug == slug;
-    });
-    this.definition[index].name = newSlug;
-    this.definition[index].slug = newSlug;
-    return this._writeSchema().catch((err) => {
-        this.definition[index].name = slug;
-        this.definition[index].slug = slug;
-        throw err;
-    });
-};
-Schema.prototype.changeColumnType = function (label, newType) {
-    var index = _.findIndex(this.definition, (el) => {
-        return el.slug == label;
-    });
-    var oldType = this.definition[index].type;
-    this.definition[index].type = newType;
-    return this._writeSchema().catch((err) => {
-        this.definition[index].type = oldType;
-        throw err;
-    });
-};
-Schema.prototype.removeColumn = function (label) {
-    var index = _.findIndex(this.definition, (el) => {
-        return el.label == label;
-    });
-    var deleted = this.definition.splice(index, 1);
-    return this._writeSchema().catch((err) => {
-        this.definition.splice(index, 0, ...deleted);
-        throw err;
-    });
-};
-// Utils --------------------------------------------------------
-Schema.prototype._writeSchema = function () {
-    return con.then((db) => {
-        return db.collection("_schema").findOneAndUpdate({ collectionSlug: this.tableSlug }, {
-            $set: {
-                fields: this.definition
-            }
+    }
+    define(tableName, tableSlug, def) {
+        var oldTableName = this.tableName;
+        var oldTableSlug = this.tableSlug;
+        var oldDef = this.definition;
+        this.tableName = tableName;
+        this.tableSlug = tableSlug;
+        this.definition = def;
+        // Create schema in RMDB, do nothing in NoSQL
+        return con.then((db) => {
+            return db.collection("_schema").insertOne({
+                collectionSlug: tableSlug,
+                collectionName: tableName,
+                fields: def
+            });
+        }).catch((err) => {
+            this.tableName = oldTableName;
+            this.tableSlug = oldTableSlug;
+            this.definition = oldDef;
+            throw err;
         });
-    });
-};
-// Initial setup of counter object for collection
-Schema.prototype._setCounter = function (collection, columnLabel) {
-    return con.then((db) => {
-        let sequenceKey = `sequences.${columnLabel}`;
-        return db.collection("_counters").findOneAndUpdate({
-            collection: collection
-        }, {
-            $set: {
-                [sequenceKey]: 0
-            }
+    }
+    addColumn(slug, type) {
+        this.definition.push({
+            name: slug,
+            slug: slug,
+            type: type
         });
-    });
-};
-// Increment counter
-Schema.prototype._incrementCounter = function (collection, columnLabel) {
-    return con.then((db) => {
-        return db.collection("_counters").findOne({
-            collection: collection
-        }).then((result) => {
-            let newSequence = result.sequences[columnLabel] + 1;
+        return this._writeSchema().catch((err) => {
+            this.definition.pop();
+            throw err;
+        });
+    }
+    addColumns(def) {
+        let oldDefinition = _.cloneDeep(this.definition);
+        this.definition = this.definition.concat(def);
+        return this._writeSchema().catch((err) => {
+            this.definition = _.cloneDeep(oldDefinition);
+            throw err;
+        });
+    }
+    renameColumn(slug, newSlug) {
+        var index = _.findIndex(this.definition, (el) => {
+            return el.slug == slug;
+        });
+        this.definition[index].name = newSlug;
+        this.definition[index].slug = newSlug;
+        return this._writeSchema().catch((err) => {
+            this.definition[index].name = slug;
+            this.definition[index].slug = slug;
+            throw err;
+        });
+    }
+    changeColumnType(slug, newType) {
+        var index = _.findIndex(this.definition, (el) => {
+            return el.slug == slug;
+        });
+        var oldType = this.definition[index].type;
+        this.definition[index].type = newType;
+        return this._writeSchema().catch((err) => {
+            this.definition[index].type = oldType;
+            throw err;
+        });
+    }
+    removeColumn(slug) {
+        var index = _.findIndex(this.definition, (el) => {
+            return el.label == slug;
+        });
+        var deleted = this.definition.splice(index, 1);
+        return this._writeSchema().catch((err) => {
+            this.definition.splice(index, 0, ...deleted);
+            throw err;
+        });
+    }
+    // Utils --------------------------------------------------------
+    _writeSchema() {
+        return con.then((db) => {
+            return db.collection("_schema").findOneAndUpdate({ collectionSlug: this.tableSlug }, {
+                $set: {
+                    fields: this.definition
+                }
+            });
+        });
+    }
+    _setCounter(collection, columnLabel) {
+        return con.then((db) => {
             let sequenceKey = `sequences.${columnLabel}`;
             return db.collection("_counters").findOneAndUpdate({
                 collection: collection
             }, {
                 $set: {
-                    [sequenceKey]: newSequence
+                    [sequenceKey]: 0
                 }
-            }).then(() => {
-                return Promise.resolve(newSequence);
             });
         });
-    });
-};
-Schema.prototype._validate = function () {
-    // Validate database schema with this.definition
-    // Return boolean
-};
+    }
+    _incrementCounter(collection, columnLabel) {
+        return con.then((db) => {
+            return db.collection("_counters").findOne({
+                collection: collection
+            }).then((result) => {
+                let newSequence = result.sequences[columnLabel] + 1;
+                let sequenceKey = `sequences.${columnLabel}`;
+                return db.collection("_counters").findOneAndUpdate({
+                    collection: collection
+                }, {
+                    $set: {
+                        [sequenceKey]: newSequence
+                    }
+                }).then(() => {
+                    return Promise.resolve(newSequence);
+                });
+            });
+        });
+    }
+    _validate() {
+        // Validate database schema with this.definition
+        // Return boolean
+    }
+}
 module.exports = function (connection) {
     con = connection;
     return Schema;
