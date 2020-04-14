f88fcf6530053c8ab6fed2c1c293f1e9c4469921
limzykenneth <limzy.kenneth@gmail.com>
2020-04-14 20:46:05 +0100

    Drop all tables despite any prior errors

--- test/mysql/utils.js
+++ test/mysql/utils.js
@@ -5,18 +5,18 @@ let utils = {};
 utils.dropTestTable = async function(connect){
 	const connection = await connect;
 
-	try{
-		await connection.execute(`DROP TABLE ${testSchema.$id}`);
-		await connection.execute("DROP TABLE _schema");
-		await connection.execute("DROP TABLE test_table");
-	} catch(err) {
+	return Promise.all([
+		connection.execute(`DROP TABLE ${testSchema.$id}`),
+		connection.execute("DROP TABLE _schema"),
+		connection.execute("DROP TABLE test_table")
+	]).catch((err) => {
 		// Error 1051 "Unknown table error", ignore it
 		// Everything else, quits with error code 1
 		if(err.errno !== 1051){
 			console.error(err);
 			process.exit(1);
 		}
-	}
+	});
 };
 
 module.exports = utils;
