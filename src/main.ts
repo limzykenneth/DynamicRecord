1bc5fafd991c3f7e9813eb7f80540fab8d01e01c
limzykenneth <limzy.kenneth@gmail.com>
2022-03-25 18:35:42 +0000

    Quick fix to typescript export

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,26 @@
+import {DynamicRecord} from "./DynamicRecord";
+
 const databaseURIRegex = /^(?<schema>.+?):\/\/(?:(?<username>.+?)(?::(?<password>.+))?@)?(?<host>.+?)(?::(?<port>\d+?))?(?:\/(?<database>.+?))?(?:\?(?<options>.+?))?$/;
 const regexResult = process.env.database_host.match(databaseURIRegex);
 
-let DynamicRecord;
+type DynamicRecordConstructor = {
+	new(options: object): DynamicRecord;
+};
+
+let DR: DynamicRecordConstructor;
 
 switch(regexResult.groups.schema){
 	case "mongodb":
 	case "mongodb+srv":
-		DynamicRecord = require("./mongodb/DynamicRecord.js");
+		DR = require("./mongodb/DynamicRecord.js");
 		break;
 
 	case "mysql":
-		DynamicRecord = require("./mysql/DynamicRecord.js");
+		DR = require("./mysql/DynamicRecord.js");
 		break;
 
 	default:
 		throw new Error("Environment not set up correctly");
 }
 
-module.exports = DynamicRecord;
+export = DR;
